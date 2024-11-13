1e5ad151f8313ce7d04ee39406f93c57fd1928ff
Tsutski <melquimendes40@gmail.com>
2024-11-13 07:56:12 -0400

    Código não era desnecessário

--- js/contact.js
+++ js/contact.js
@@ -2,6 +2,7 @@ const btn_loc = document.querySelector("#btn-loc");
 const mapa = document.querySelector("#mapa");
 
 const btn_talk = document.querySelector("#btn-talk");
+const btn_cancel = document.querySelector("#btn-cancel");
 const blocks = document.querySelectorAll(".content");
 const email_box = document.querySelector("#email-box");
 
@@ -26,4 +27,15 @@ btn_talk.addEventListener("click", () => {
     })
     email_box.style.display = 'flex';
     btn_loc.style.display = 'none';
+})
+
+btn_cancel.addEventListener("click", () => {
+    blocks.forEach((block) =>{
+        if (block.id != "email-box"){
+            block.style.display = 'block';
+        }else {
+            block.style.display = 'none';
+        }
+        btn_loc.style.display = 'flex';
+    })
 })
